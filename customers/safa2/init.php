5e4ffc94d123e5605c511472a4529591d5259674
ForsanNemah <forsan20172017@gmail.com>
2023-06-10 04:00:09 +0300

    edit  lp init

--- customers/safa2/init.php
+++ customers/safa2/init.php
@@ -10,11 +10,7 @@ include '../gmr/connect.php';
 
 
 
-$host="localhost";
-$db_user="root";
-$db_password="";
-$db_name="getmonyfromgmail";
-$con = mysqli_connect($host,$db_user,$db_password,$db_name);
+ 
 
  if($con){
 echo "ok";
