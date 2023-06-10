edfca3bf0b80cfd4288cd9903152dab1a0aa0c28
ForsanNemah <forsan20172017@gmail.com>
2023-06-10 04:07:44 +0300

    init fix

--- customers/safa2/init.php
+++ customers/safa2/init.php
@@ -1,6 +1,6 @@
 <?php
 
-include '../gmr/connect.php';
+include '../../lp_init.php';
 
 
 
---
+++ lp_init.php
@@ -0,0 +1,30 @@
+<?php
+
+include '../lp_init.php';
+
+
+
+
+/*
+$host=localhost;
+$db_user=root;
+$db_password="";
+$db_name=getmonyfromgmail;
+
+*/
+$host=localhost;
+$db_name = 'mysql:host=localhost;dbname=u122541238_efg';
+$db_user     = 'u122541238_efg_root';
+$db_password   = 'Forsan@2023';
+
+
+
+
+
+
+
+
+
+$con = mysqli_connect($host,$db_user,$db_password,$db_name);
+
+?>
