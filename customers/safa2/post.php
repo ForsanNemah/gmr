f8580363373ad97ef924c270f588bc190ce6b54d
ForsanNemah <forsan20172017@gmail.com>
2023-06-13 05:42:50 +0300

    add

--- customers/safa2/post.php
+++ customers/safa2/post.php
@@ -16,7 +16,7 @@ $url=$_POST['url'];
 $customer=$_POST['customer'];
 
 echo $username.$phone.$url.$customer."<br>"; 
-echo send_mail("forsan20172017@gmail.com","NewMapReview@pscye.com","Psc@2023",$customer,$url,"smtp.hostinger.com","465");
+//echo send_mail("forsan20172017@gmail.com","NewMapReview@pscye.com","Psc@2023",$customer,$url,"smtp.hostinger.com","465");
 
 
 
@@ -63,7 +63,7 @@ try {
 
 	if(mysqli_query($con,$sql)){
 
-        //echo send_mail("forsan20172017@gmail.com","NewMapReview@pscye.com","Psc@2023",$customer,$url,"smtp.hostinger.com","465");
+        echo send_mail("forsan20172017@gmail.com","NewMapReview@pscye.com","Psc@2023",$customer,$url,"smtp.hostinger.com","465");
 
         echo "<br> ok inserted ";
         
