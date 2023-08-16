c2c50229f8a5b9df8aa7771cfb22480c5bfbcdcd
ForsanNemah <forsan20172017@gmail.com>
2023-08-16 11:58:00 +0300

    added

---
+++ pscsms/head/index.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="head/css/owl.carousel.min.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="head/css/bootstrap.min.css">
+    
+    <!-- Style -->
+    <link rel="stylesheet" href="head/css/style.css">
+
+    
+  </head>
+  <body>
+
+
+    <div class="site-mobile-menu">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+    
+  
+
+ 
+  
+    <img src="head/images/hero_1.jpg" class="img-fluid" alt="Responsive image"  >
+ 
+ 
+
+
+   
+
+
+    <script src="head/js/jquery-3.3.1.min.js"></script>
+    <script src="head/js/popper.min.js"></script>
+    <script src="head/js/bootstrap.min.js"></script>
+    <script src="head/js/jquery.sticky.js"></script>
+    <script src="head/js/main.js"></script>
+  </body>
+</html>
+
+
+<style>
+
+
+ 
+</style>
